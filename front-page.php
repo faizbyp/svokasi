3131295caf3e1b00ed1df4d69e8e4d23cd7ab540
faizbyp <faizbyp@gmail.com>
2023-01-27 22:44:25 +0700

    centering hero text

--- front-page.php
+++ front-page.php
@@ -1,9 +1,11 @@
 <?php get_header();?>
 
-<div class="container-fluid hero">
-  <div class="container">
-    a
+<section class="container-fluid d-flex hero">
+  <div class="row align-items-center w-100 flex-grow-1">
+    <div class="col">
+      <h1 class="text-accent text-center fw-bold"><span class="text-white">“To be</span> The Premiere Choice <span class="text-white">for</span><br>Higher Vocational Education”</h1>
+    </div>
   </div>
-</div>
+</section>
 
 <?php get_footer();?>
