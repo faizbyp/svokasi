37edc68af54309f262bf33a66bde675f3009ecb1
faizbyp <faizbyp@gmail.com>
2023-02-01 23:29:37 +0700

    finish card and add explore section

--- front-page.php
+++ front-page.php
@@ -23,42 +23,134 @@
   </div>
 </section>
 
-<section class="container-lg py-5 informasi">
-  <div class="row">
-    <h2 class="col-12 text-center fw-bold">Informasi Terbaru</h2>
-    <div class="col-12 text-center">
-      <a href="#" class="btn btn-secondary fw-semibold"><img src="wp-content/themes/svokasi/assets/img/icon berita.svg" alt=""> Berita</a>
-      <a href="#" class="btn btn-grey text-secondary fw-semibold"><img src="wp-content/themes/svokasi/assets/img/icon horn.svg" alt=""> Pengumuman</a>
-      <a href="#" class="btn btn-grey text-secondary fw-semibold"><img src="wp-content/themes/svokasi/assets/img/icon calendar.svg" alt=""> Agenda</a>
+<section class="container-fluid py-5 informasi">
+  <section class="container-lg">
+    <div class="row">
+      <h2 class="col-12 text-center fw-bold">Informasi Terbaru</h2>
+      <div class="col-12 text-center">
+        <a href="#" class="btn btn-secondary fw-semibold"><img src="wp-content/themes/svokasi/assets/img/icon berita.svg" alt=""> Berita</a>
+        <a href="#" class="btn btn-grey text-secondary fw-semibold"><img src="wp-content/themes/svokasi/assets/img/icon horn.svg" alt=""> Pengumuman</a>
+        <a href="#" class="btn btn-grey text-secondary fw-semibold"><img src="wp-content/themes/svokasi/assets/img/icon calendar.svg" alt=""> Agenda</a>
+      </div>
     </div>
-  </div>
+  
+    <div class="row my-3">
+      <div class="col-12 col-lg-4 mb-4">
+        <div class="card">
+          <img src="wp-content/themes/svokasi/assets/img/image 9.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text fw-bold">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-outline-primary rounded-pill">Berita</button>
+            <button class="btn btn-outline-primary rounded-pill">Juara</button>
+            <span>5 November 2022</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-4 mb-4">
+        <div class="card">
+          <img src="wp-content/themes/svokasi/assets/img/image 7.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text fw-bold">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-outline-primary rounded-pill">Berita</button>
+            <button class="btn btn-outline-primary rounded-pill">Juara</button>
+            <span>5 November 2022</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-4 mb-4">
+        <div class="card">
+          <img src="wp-content/themes/svokasi/assets/img/image 8.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text fw-bold">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-outline-primary rounded-pill">Berita</button>
+            <button class="btn btn-outline-primary rounded-pill">Juara</button>
+            <span>5 November 2022</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-4 mb-4">
+        <div class="card">
+          <img src="wp-content/themes/svokasi/assets/img/image 10.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text fw-bold">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-outline-primary rounded-pill">Berita</button>
+            <button class="btn btn-outline-primary rounded-pill">Juara</button>
+            <span>5 November 2022</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-4 mb-4">
+        <div class="card">
+          <img src="wp-content/themes/svokasi/assets/img/image 11.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text fw-bold">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-outline-primary rounded-pill">Berita</button>
+            <button class="btn btn-outline-primary rounded-pill">Juara</button>
+            <span>5 November 2022</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-4 mb-4">
+        <div class="card">
+          <img src="wp-content/themes/svokasi/assets/img/image 12.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text fw-bold">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-outline-primary rounded-pill">Berita</button>
+            <button class="btn btn-outline-primary rounded-pill">Juara</button>
+            <span>5 November 2022</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</section>
 
-  <div class="row my-3">
-    <div class="col-12 col-lg-4">
-      <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <button class="btn btn-outline-primary rounded-pill">Berita</button>
-          <button class="btn btn-outline-primary rounded-pill">Juara</button>
-          <span>5 November 2022</span>
-        </div>
+<section class="container-fluid py-5 explore">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-lg-4 my-3">
+        <a href="#" class="card">
+          <img src="wp-content/themes/svokasi/assets/img/image 14.png" class="card-img" alt="...">
+          <div class="card-img-overlay d-flex">
+            <span class="text-center m-auto">
+              <h1 class="fw-bold text-accent">E-Brosur</h1>
+              <p class="text-white">Download E-Brosur SV IPB</p>
+            </span>
+          </div>
+        </a>
       </div>
-    </div>
-    <div class="col-12 col-lg-4">
-      <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
+      <div class="col-12 col-lg-8 my-3">
+        <a href="#" class="card">
+          <img src="wp-content/themes/svokasi/assets/img/image 15.png" class="card-img" alt="...">
+          <div class="card-img-overlay d-flex">
+            <span class="text-center m-auto">
+              <h1 class="fw-bold text-accent">Virtual Tour</h1>
+              <p class="text-white">Jelajah Kampus Virtual</p>
+            </span>
+          </div>
+        </a>
       </div>
-    </div>
-    <div class="col-12 col-lg-4">
-      <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
+      <div class="col-12 col-lg-7 my-3">
+        <a href="#" class="card">
+          <img src="wp-content/themes/svokasi/assets/img/image 16.png" class="card-img" alt="...">
+          <div class="card-img-overlay d-flex">
+            <span class="text-center m-auto">
+              <h1 class="fw-bold text-accent">Pendaftaran</h1>
+              <p class="text-white">Informasi Seputar Pendaftaran</p>
+            </span>
+          </div>
+        </a>
+      </div>
+      <div class="col-12 col-lg-5 my-3">
+        <a href="#" class="card">
+          <img src="wp-content/themes/svokasi/assets/img/image 17.png" class="card-img" alt="...">
+          <div class="card-img-overlay d-flex">
+            <span class="text-center m-auto">
+              <h1 class="fw-bold text-accent">Produk Inovasi</h1>
+              <p class="text-white">Inovasi SV IPB</p>
+            </span>
+          </div>
+        </a>
       </div>
     </div>
   </div>
