5d403b013951b92a3cc544701af2a82e00114323
faizbyp <faizbyp@gmail.com>
2023-02-14 21:33:51 +0700

    start footer

--- footer.php
+++ footer.php
@@ -1,4 +1,32 @@
+<footer class="container-fluid bg-primary py-5">
+  <footer class="container-lg">
+    <div class="row">
+      <div class="col-12 col-lg-4">
+        <img src="wp-content/themes/svokasi/assets/img/logo footer.png" alt="">
+        <h2 class="text-white fw-bold">Tautan</h2>
+        <a href="#" class="text-white text-decoration-none d-block">Website IPB Official</a>
+        <a href="#" class="text-white text-decoration-none d-block">SIMAK IPB</a>
+        <a href="#" class="text-white text-decoration-none d-block">IPB Online Course</a>
+        <a href="#" class="text-white text-decoration-none d-block">Sistem Informasi Terintegrasi IPB</a>
+        <a href="#" class="text-white text-decoration-none d-block">Admisi IPB</a>
+        <a href="#" class="text-white text-decoration-none d-block">IPB Apps</a>
+        <a href="#" class="text-white text-decoration-none d-block">IPB University Repository</a>
+      </div>
 
+      <div class="col-12 col-lg-4">
+        <h2 class="text-white fw-bold">Our Contact</h2>
+        <p class="text-white">KAMPUS BOGOR - Jl. Raya Pajajaran, Kota Bogor, Jawa Barat 16128</p>
+        <p class="text-white">KAMPUS SUKABUMI - Jl. Sarasa No. 45, Babakan, Kec. Cibeurum, Kota Sukabumi, Jawa Barat 431142</p>
+        <p class="text-white">(0251) 8376845</p>
+        <p class="text-white">sv@apps.ipb.ac.id</p>
+      </div>
+
+      <div class="col-12 col-lg-4">
+        <form action=""></form>
+      </div>
+    </div>
+  </footer>
+</footer>
 
 <?php wp_footer(); ?>
 
